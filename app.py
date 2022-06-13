45c9bac1f95db5345e3d089cb5523950edd07aa4
Breno Albuquerque <brenoralbu@hotmail.com>
2022-06-13 08:54:55 -0300

    chore: Create first connection to sqlite3 DB

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Config DB:
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+# Initialize DB:
+db = SQLAlchemy(app)
+# Create DB Model:
+class TestTable(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
 
 @app.route('/')
 def index():
