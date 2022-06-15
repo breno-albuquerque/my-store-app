324ca1c09bdba697f79e1b6f2baa4e67d757eefb
Breno Albuquerque <brenoralbu@hotmail.com>
2022-06-15 18:23:05 -0300

    feat: Buttons now are dinamicaly created and added to main.html template

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,8 @@
 const mainForm = document.getElementById('main-form');
 const flashMsg = document.getElementById('flash-main');
 
+const categories = ['Desktop', 'Notebook', 'Monitors', 'Macbook', 'Tablet', 'Ipad', 'Keyboard', 'Mouse', 'Smartphone', 'Iphone', 'Smart Tv', 'Alexa', 'Playstation 5', 'Xbox series x']
+
 function handleCartBtnClick(event) {
     event.preventDefault();
 
@@ -14,6 +16,18 @@ function handleCartBtnClick(event) {
 }
 
 window.onload = () => {
+    categories.forEach(item => {
+        const btn = document.createElement('button');
+        btn.name = "category";
+        btn.className = "main-btn";
+        btn.value = item;
+        btn.type = "submit";
+        btn.id = `${item.toLowerCase()}-btn`;
+        btn.innerText = item;
+
+        mainForm.appendChild(btn);
+    });
+
     if (!flashMsg) return;
 
     setTimeout(() => {
