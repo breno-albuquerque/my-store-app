9c5504e0721569fb44802a0b937c7eaa36c0701f
Breno Albuquerque <brenoralbu@hotmail.com>
2022-06-15 09:11:39 -0300

    feat: Add prices and total cart price

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,20 +1,36 @@
 const removeBtns = Array.from(document.getElementsByClassName('remove-btn'));
 const cartSection = document.getElementById('cart-section');
-const flashProduct = document.getElementById('flash-product')
+const flashProduct = document.getElementById('flash-product');
+let productsPrices = Array.from(document.getElementsByClassName('product-price'));
+const totalPrice = document.getElementById('total-price');
 
 const handleRemove = async ({ target }) => {
     const cartItem = target.parentElement;
     cartSection.removeChild(cartItem);
 
-    flashProduct.style.display = "block";
+    productsPrices = Array.from(document.getElementsByClassName('product-price'));
+    const total = productsPrices.reduce((acc, curr) => {
+        console.log(curr)
+         return parseFloat(curr.innerText) + acc
+     }, 0);
+     totalPrice.innerText = `Total: ${total.toFixed(2)}`;
 
+    flashProduct.style.display = "block";
     setTimeout(() => {
        flashProduct.style.display = "none";
-   }, 1500);
+    }, 1500);
 
     await fetch(`http://localhost:5000/deleteCart?id=${cartItem.id}`, {
         method: "DELETE",
     });
 }
 
-removeBtns.forEach(btn => btn.addEventListener('click', handleRemove)); 
+removeBtns.forEach(btn => btn.addEventListener('click', handleRemove));
+
+window.onload = () => {
+    const total = productsPrices.reduce((acc, curr) => {
+        return parseFloat(curr.innerText) + acc
+    }, 0);
+
+    totalPrice.innerText = `Total: ${total.toFixed(2)}`;
+}
