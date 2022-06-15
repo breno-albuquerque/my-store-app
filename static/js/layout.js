b273108febd90156bdde87a5ad9b8e05866b77e9
Breno Albuquerque <brenoralbu@hotmail.com>
2022-06-15 19:08:58 -0300

    feat: Use jinja to dinamicaly set nav item bg color

--- static/js/layout.js
+++ static/js/layout.js
@@ -1,6 +1,8 @@
 const layoutBtns = Array.from(document.getElementsByClassName('layout-btn'));
 const layoutForm = document.getElementById('layout-form');
 
+const anchors = [searchAnchor, cartAnchor, logoutAnchor];
+
 function handleLayoutBtnClick(event) { 
     event.preventDefault();
     
@@ -8,4 +10,4 @@ function handleLayoutBtnClick(event) {
     layoutForm.action = '/' + value;
 }
 
-layoutBtns.forEach(btn => btn.addEventListener('click', handleLayoutBtnClick))
+layoutBtns.forEach(btn => btn.addEventListener('click', handleLayoutBtnClick));
